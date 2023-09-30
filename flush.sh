665c925005e5c6d49b45dc0ebffbc45e53aa21ca
Vaisakh B <vaisakh.b@amazecodes.com>
2023-09-30 12:42:26 +0530

    some changes

--- flush.sh
+++ flush.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 
-###### system ######
-mkdir -p ~/Pictures/Screenshots
+										###### system default ######
+
 mkdir -p /Users/${USER}/.config
 
 
-###### plist configs ######
+										###### plist configs ######
+
+# iterm configs
 sudo ln -s ${PWD}/src/plists/com.googlecode.iterm2.plist ~/Library/Preferences/
 # enabling key repeat for vscode
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
-# setting Kcreenshot path
+# setting Screenshot path
+mkdir -p ~/Pictures/Screenshots
 defaults write com.apple.screencapture location /Users/${USER}/Pictures/Screenshots
 # setting Key repeat rate
 defaults write -g KeyRepeat -int 5
@@ -20,18 +23,14 @@ defaults write -g InitialKeyRepeat -int 15
 defaults write com.apple.dock "tilesize" -int "38" && killall Dock
 
 
+										#### symlinks and configs ####
+
 ###### iterm ######
 sudo ln -s ${PWD}/src/config/iterm2 /Users/${USER}/.config
-
-
 ###### nvim ######
 sudo ln -s ${PWD}/src/config/nvim /Users/${USER}/.config
-
-
 ###### qBittorrent ######
 sudo ln -s ${PWD}/src/config/qBittorrent /Users/${USER}/.config
-
-
 # bits installination
 sudo mkdir -p /Library/Developer/CommandLineTools/usr/include/c++/v1
 sudo mkdir -p /usr/local/include/
